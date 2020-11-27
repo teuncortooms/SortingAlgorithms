82aa63fd006a56f57ebcc6269c5d33f3910ff613
Teun Cortooms <teuncortooms@gmail.com>
2020-11-27 21:58:59 +0100

    Counting sorter

--- src/main/java/CountingSorter.java
+++ src/main/java/CountingSorter.java
@@ -1,26 +1,26 @@
 public class CountingSorter {
 
-    public int[] sort(int[] A, int upperValueBound) {
+    public int[] sort(int[] input, int upperValueBound) {
 
-        int[] B = new int[A.length];
-        int[] C = new int[upperValueBound];
+        int[] output = new int[input.length];
+        int[] counts = new int[upperValueBound];
 
-        for (int i = 0; i < A.length; i++){
-            int value = A[i];
-            C[value]++;
+        for (int value : input) {
+            counts[value]++;
         }
 
-        for (int i = 1; i < C.length; i++){
-            C[i] = C[i] + C[i-1];
+        for (int i = 1; i < counts.length; i++){
+            counts[i] = counts[i] + counts[i-1];
         }
 
-        for (int i = A.length-1; i <= 0; i--){
-            int value = A[i];
-            int newIndex = C[value];
-            B[newIndex] = value;
-            C[value]--; // decrement index by 1
+        for (int i = input.length-1; i >= 0; i--){
+            int value = input[i];
+            int newIndex = counts[value] - 1; // -1 because indexed at 0
+            output[newIndex] = value;
+            // remove occurrence and decrement next index by 1
+            counts[value]--;
         }
 
-        return B;
+        return output;
     }
 }
